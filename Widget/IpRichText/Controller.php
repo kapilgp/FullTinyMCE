240213e37d980bd02fbb3cb7f00065d4b465d6a7
Mangirdas <mangirdas@impresspages.org>
2014-03-13 08:48:48 +0200

    first commit

---
+++ Widget/IpRichText/Controller.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package ImpressPages
+
+ *
+ */
+namespace Plugin\FullTinyMCE\Widget\IpRichText;
+
+
+
+
+class Controller extends \Ip\WidgetController{
+
+
+    public function getTitle() {
+        return __('Rich text', 'ipAdmin');
+    }
+    
+}
