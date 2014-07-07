9559f61e1d048d00a04d63ddbdb7f99bf276202b
Mangirdas Skripka <mangirdas@impresspages.org>
2014-07-07 23:15:30 +0300

    Update IpRichText.js

--- Widget/IpRichText/assets/IpRichText.js
+++ Widget/IpRichText/assets/IpRichText.js
@@ -12,7 +12,7 @@ var IpWidget_IpRichText;
 
         this.init = function($widgetObject, data) {
             var customTinyMceConfig = ipTinyMceConfig();
-            customTinyMceConfig.plugins = customTinyMceConfig.plugins + " advlist autolink lists link image charmap print preview anchor searchreplace visualblocks code fullscreen insertdatetime media table contextmenu paste";
+            customTinyMceConfig.plugins = customTinyMceConfig.plugins + " advlist autolink lists link image charmap print preview anchor searchreplace visualblocks code fullscreen insertdatetime media table contextmenu";
             customTinyMceConfig.toolbar = "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image";
             customTinyMceConfig.menubar = true;
 
